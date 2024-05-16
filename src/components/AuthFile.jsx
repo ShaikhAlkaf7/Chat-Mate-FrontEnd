5fff0988be1c5e509f3d62b8ebb9c48155a5f695
Shaikh Alkaf <shaikhalkafofficial@gmail.com>
2024-05-16 19:30:57 +0530

    a

--- src/components/AuthFile.jsx
+++ src/components/AuthFile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const AuthFile = () => {
+  const navigate = useNavigate();
   const { user } = useSelector((state) => state?.user);
 
-  return <>{user?.token ? <Outlet /> : null}</>;
+  return <>{user?.token ? <Outlet /> : navigate("/login")}</>;
 };
 
 export default AuthFile;
